97e696c0c32cd72cab6f01e308490e216383abaf
wingered <wingered@gmail.com>
2017-11-27 01:02:11 -0600

    Initial check in of sales tool

---
+++ Sales Tool/src/com/arjun/git/Main.java
@@ -0,0 +1,22 @@
+package com.arjun.git;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SalesData obj = new SalesData();
+		
+		displayGreeting();
+		obj.display();
+		
+		
+	}
+	
+	private static void displayGreeting()
+	{
+		System.out.println("Hello happy sales poeple");;
+		System.out.println("This application shows sales data");
+	}
+
+}
---
+++ Sales Tool/src/com/arjun/git/SalesData.java
@@ -0,0 +1,12 @@
+package com.arjun.git;
+
+public class SalesData {
+
+	
+	int[] val = {10,20,30};
+	
+	public void display()
+	{
+		System.out.println("This also prints out a message from other class");
+	}
+}
